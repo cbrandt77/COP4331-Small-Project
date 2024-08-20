06b8bf27381c27c894ddf72d1c27961741c4ea9e
CalebB <ca465171@ucf.edu>
2024-08-20 12:13:02 -0400

    Added NPM. Figured out how to link scripts to page.

---
+++ src/main/js/main.js
@@ -0,0 +1,6 @@
+function main() {
+    console.log("fired")
+    const els = document.getElementsByClassName("scripttest");
+    console.log(els)
+    els[0].style.color = "#3385ff";
+}
--- src/main/php/index.php
+++ src/main/php/index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<head>
-    <title>Contact Manager</title>
-</head>
-<body>
-<p>This is a paragraph</p>
-<?php
-    print("yeet");
-?>
-</body>
+    <head>
+        <title>Contact Manager</title>
+        <script src="../js/main.js"></script>
+    </head>
+    <body>
+        <p class="scripttest">This is a paragraph</p>
+        <?php
+            print("yeet");
+        ?>
+    <button onclick="main()">Button</button>
+    </body>
 </html>
