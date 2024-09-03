c423da72f2ad979231482d01fdcf5a690719e112
CalebB <ca465171@ucf.edu>
2024-09-03 11:16:49 -0400

    Add Lamp Stack Demo

---
+++ main/js/main.js
@@ -0,0 +1,32 @@
+"use strict";
+console.log("initialized main script");
+console.log(document.forms);
+function submitLoginForm() {
+    console.log("runnginggg");
+    const loginform = document.forms[0];
+    if (loginform == null) {
+        console.log("null form");
+        return;
+    }
+    const data = new FormData(loginform);
+    fetch("/LAMPAPI/Login.php", {
+        method: "POST",
+        body: data
+    }).then(changeOtherThing, null);
+}
+function changeOtherThing(response) {
+    function setResponseAreaText(text) {
+        const elementById = document.getElementById("responsearea");
+        elementById && (elementById.innerText = text);
+    }
+    response.body && response.text().then(setResponseAreaText, setResponseAreaText);
+}
+document.addEventListener("load", initLoginForm);
+function initLoginForm(event) {
+    console.log("runninginit");
+    const loginform = document.forms.namedItem("loginform");
+    loginform ? loginform.addEventListener("submit", (event) => {
+        event.preventDefault();
+        submitLoginForm();
+    }) : console.log("loginform is null");
+}
