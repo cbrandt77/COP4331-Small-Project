c16bf06827c01ca04524fc65115844c486a998d9
CalebB <ca465171@ucf.edu>
2024-09-12 15:19:05 -0400

    Fix login.ts

---
+++ main/ts/util/cookies.ts
@@ -0,0 +1,8 @@
+import {LoginConfirmedPacket} from "~types/packets";
+import JSCookieLib from "js-cookie";
+
+export function onLoginSuccess(packet: LoginConfirmedPacket) {
+    const userId = packet.id;
+    JSCookieLib.set('user_id', userId.toString())
+    document.location.href = "contacts.html"
+}
