5398f1118fee4e2b5182b726885013b1b66b7ebc
manaskorada <153758442+manaskorada@users.noreply.github.com>
2024-09-17 12:22:20 -0400

    Update working now

--- main/LAMPAPI/Update.php
+++ main/LAMPAPI/Update.php
@@ -1,34 +1,34 @@
 <?php
 include '../php/global_functions.php';
-include '../php/packets.php';
 
-setCORSHeaders();
-$inData = expectPacketType(new ContactEditPacket());
+header("Access-Control-Allow-Origin: *");
+$inData = getRequestInfo();
+
 
 $conn = getSqlConn();
 
-if ($conn->connect_error) {
-    returnJsonHttpResponse(500, new ErrorPacket(1, $conn->connect_error));
-} else {
+if ($conn->connect_error)
+{
+    returnWithError( $conn->connect_error );
+}
+else
+{
     $stmt = $conn->prepare("UPDATE Contacts SET Name=?, Phone=?, Email=? WHERE ID=? AND UserID=?");
-    $stmt->bind_param("sssii", $inData->name, $inData->phone_number, $inData->email_address, $inData->contact_id, $inData->user_id);
+    $stmt->bind_param("sssii", $inData["name"], $inData["phone"], $inData["email"], $inData["contactId"], $inData["userId"]);
     $stmt->execute();
-    if (!$stmt->get_result()) {
-        $err = $stmt->error;
-        $stmt->close();
-        returnJsonHttpResponse(200, new ErrorPacket(4, $err));
-    } else {
-        $stmt->close();
-    }
-
-
-    $stmt = $conn->prepare("SELECT * FROM Contacts WHERE ID=? AND UserID=?");
-    $stmt->bind_param("ii", $inData->contact_id, $inData->user_id);
-    $stmt->execute();
-
-    $updatedContact = $stmt->get_result()->fetch_object('\SqlContact');
     $stmt->close();
     $conn->close();
+    returnWithError("");
+}
 
-    returnJsonHttpResponse(200, OutgoingContactObject::fromSqlContact($updatedContact));
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
 }
