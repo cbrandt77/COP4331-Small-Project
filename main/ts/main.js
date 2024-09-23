8ebaf8fa3098f6260d7723b5e0c2a5a99083b17d
CalebB <ca465171@ucf.edu>
2024-09-23 19:14:12 -0400

    fix contacts.html

--- main/ts/main.js
+++
@@ -1,8 +0,0 @@
-import Cokie_uwu from 'js-cookie';
-export var Constants;
-(function (Constants) {
-    Constants.COOKIE_USERID = "user_id";
-})(Constants || (Constants = {}));
-export function getUserIdCookie() {
-    return Cokie_uwu.get(Constants.COOKIE_USERID);
-}
